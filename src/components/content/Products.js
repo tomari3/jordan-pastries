d5322c1db87df57c06048a4ffa18eebe91ec1292
Tom Ari <freak.tom3@gmail.com>
2022-04-01 17:32:27 +0300

    Gutendex test

--- src/components/content/Products.js
+++ src/components/content/Products.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { item } from "./example";
+import PropTypes from "prop-types";
 
-const ProductsCard = () => {
+const ProductsCard = ({ title, image, author }) => {
   return (
     <div>
-      <p>card</p>
+      <h1>{title}</h1>
+      <p>{author}</p>
+      <img src={image} height={100} width={100} />
     </div>
   );
 };
 
+ProductsCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  author: PropTypes.string.isRequired,
+};
+
 const ProductsGallery = () => {
-  return (
-    <div>
-      <ProductsCard />
-    </div>
-  );
+  const cardList = item.results.map((i) => {
+    return (
+      <ProductsCard
+        key={i.id}
+        title={i.title}
+        author={i.authors[0].name}
+        image={i.formats["image/jpeg"]}
+      />
+    );
+  });
+  return <div>{cardList}</div>;
 };
 
 const ProductsFilterSort = () => {
@@ -36,14 +52,17 @@ const ProductsFilterSort = () => {
 };
 
 const Products = () => {
+  console.log(item);
   return (
     <main data-testid="products">
       <div>
         <h1>All Products</h1>
         <p>cant decide?</p>
       </div>
-      <ProductsFilterSort />
-      <ProductsGallery />
+      <div>
+        <ProductsFilterSort />
+        <ProductsGallery />
+      </div>
     </main>
   );
 };
---
+++ src/components/content/example.js
@@ -0,0 +1,84 @@
+export const item = {
+  count: 2,
+  next: null,
+  previous: null,
+  results: [
+    {
+      id: 1400,
+      title: "Great Expectations",
+      authors: [
+        {
+          name: "Dickens, Charles",
+          birth_year: 1812,
+          death_year: 1870,
+        },
+      ],
+      translators: [],
+      subjects: [
+        "Benefactors -- Fiction",
+        "Bildungsromans",
+        "England -- Fiction",
+        "Ex-convicts -- Fiction",
+        "Man-woman relationships -- Fiction",
+        "Orphans -- Fiction",
+        "Revenge -- Fiction",
+        "Young men -- Fiction",
+      ],
+      bookshelves: ["Best Books Ever Listings"],
+      languages: ["en"],
+      copyright: false,
+      media_type: "Text",
+      formats: {
+        "application/x-mobipocket-ebook":
+          "https://www.gutenberg.org/ebooks/1400.kindle.images",
+        "application/epub+zip":
+          "https://www.gutenberg.org/ebooks/1400.epub.images",
+        "application/rdf+xml": "https://www.gutenberg.org/ebooks/1400.rdf",
+        "text/plain; charset=utf-8":
+          "https://www.gutenberg.org/files/1400/1400-0.txt",
+        "image/jpeg":
+          "https://www.gutenberg.org/cache/epub/1400/pg1400.cover.small.jpg",
+        "text/html": "https://www.gutenberg.org/ebooks/1400.html.images",
+        "text/html; charset=utf-8":
+          "https://www.gutenberg.org/files/1400/1400-h.zip",
+      },
+      download_count: 14283,
+    },
+    {
+      id: 8608,
+      title: "Great Expectations",
+      authors: [
+        {
+          name: "Dickens, Charles",
+          birth_year: 1812,
+          death_year: 1870,
+        },
+      ],
+      translators: [],
+      subjects: [
+        "Benefactors -- Fiction",
+        "Bildungsromans",
+        "England -- Fiction",
+        "Ex-convicts -- Fiction",
+        "Man-woman relationships -- Fiction",
+        "Orphans -- Fiction",
+        "Revenge -- Fiction",
+        "Young men -- Fiction",
+      ],
+      bookshelves: ["Best Books Ever Listings"],
+      languages: ["en"],
+      copyright: true,
+      media_type: "Sound",
+      formats: {
+        "audio/mpeg": "http://www.gutenberg.org/files/8608/mp3/8608-094.mp3",
+        "application/zip": "http://www.gutenberg.org/files/8608/8608-mp3.zip",
+        "text/html; charset=iso-8859-1":
+          "http://www.gutenberg.org/files/8608/8608-index.htm",
+        "text/plain; charset=us-ascii":
+          "http://www.gutenberg.org/files/8608/8608-readme.txt",
+        "application/rdf+xml": "http://www.gutenberg.org/ebooks/8608.rdf",
+      },
+      download_count: 37,
+    },
+  ],
+};
